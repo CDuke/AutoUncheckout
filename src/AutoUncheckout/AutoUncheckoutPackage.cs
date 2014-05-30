75db42fcaa57c65cb7e40467455635fdc0f128bf
Kulikov Denis <cdbiznes@gmail.com>
2014-05-30 14:35:26 +0400

    Initial commit

---
+++ src/AutoUncheckout/AutoUncheckoutPackage.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using IServiceProvider = System.IServiceProvider;
+
+namespace KulikovDenis.AutoUncheckout
+{
+	/// <summary>
+	/// This is the class that implements the package exposed by this assembly.
+	///
+	/// The minimum requirement for a class to be considered a valid package for Visual Studio
+	/// is to implement the IVsPackage interface and register itself with the shell.
+	/// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+	/// to do it: it derives from the Package class that provides the implementation of the 
+	/// IVsPackage interface and uses the registration attributes defined in the framework to 
+	/// register itself and its components with the shell.
+	/// </summary>
+	// This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
+	// a package.
+	[PackageRegistration(UseManagedResourcesOnly = true)]
+	// This attribute is used to register the information needed to show this package
+	// in the Help/About dialog of Visual Studio.
+	[InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+	[Guid(GuidList.guidAutoUncheckoutPkgString)]
+	[ProvideAutoLoad(ContextGuids.vsContextGuidNoSolution)]
+	public sealed class AutoUncheckoutPackage : Package
+	{
+		private Events _events;
+		private DocumentEvents _documentEvents;
+
+		/// <summary>
+		/// Default constructor of the package.
+		/// Inside this method you can place any initialization code that does not require 
+		/// any Visual Studio service because at this point the package object is created but 
+		/// not sited yet inside Visual Studio environment. The place to do all the other 
+		/// initialization is the Initialize method.
+		/// </summary>
+		public AutoUncheckoutPackage()
+		{
+			Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
+		}
+
+
+
+		/////////////////////////////////////////////////////////////////////////////
+		// Overridden Package Implementation
+		#region Package Members
+
+		/// <summary>
+		/// Initialization of the package; this method is called right after the package is sited, so this is the place
+		/// where you can put all the initialization code that rely on services provided by VisualStudio.
+		/// </summary>
+		protected override void Initialize()
+		{
+			Debug.WriteLine (string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
+			base.Initialize();
+			var dte = (DTE)GetService(typeof(DTE));
+			_events = dte.Events;
+			_documentEvents = _events.DocumentEvents;
+			_documentEvents.DocumentSaved += DocumentEvents_DocumentSaved;
+
+		}
+
+		private void DocumentEvents_DocumentSaved(Document document)
+		{
+			try
+			{
+				var workspaces = Workstation.Current.GetAllLocalWorkspaceInfo();
+				if (workspaces != null && workspaces.Length > 0)
+				{
+					var userLogin = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+					var currentWorkspace = workspaces.FirstOrDefault(w => w.Computer == Environment.MachineName && w.OwnerName == userLogin);
+					if (currentWorkspace != null)
+					{
+						var tfs = new TfsTeamProjectCollection(currentWorkspace.ServerUri);
+						var vcs = tfs.GetService<VersionControlServer>();
+						var fileInfoItem = vcs.GetItem(document.FullName);
+
+						if (fileInfoItem != null)
+						{
+							var md5 = new MD5CryptoServiceProvider();
+							using (var fileStream = new FileStream(document.FullName, FileMode.Open, FileAccess.Read))
+							{
+								var currentHash = md5.ComputeHash(fileStream);
+								var hashEquals = fileInfoItem.HashValue.SequenceEqual(currentHash);
+								if (hashEquals)
+								{
+									var w = vcs.GetWorkspace(currentWorkspace);
+									var fileInfo = new FileInfo(document.FullName);
+									if (!fileInfo.IsReadOnly)
+									{
+										w.Undo(document.FullName);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			catch
+			{}
+		}
+
+		#endregion
+
+	}
+}
---
+++ src/AutoUncheckout/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace KulikovDenis.AutoUncheckout
+{
+    static class GuidList
+    {
+        public const string guidAutoUncheckoutPkgString = "1954660f-1535-4694-9e6d-c27e8eb476a4";
+        public const string guidAutoUncheckoutCmdSetString = "d859909f-3a82-408f-b5d0-833a39b8de2e";
+
+        public static readonly Guid guidAutoUncheckoutCmdSet = new Guid(guidAutoUncheckoutCmdSetString);
+    };
+}
